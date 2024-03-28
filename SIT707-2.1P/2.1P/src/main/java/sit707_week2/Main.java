5caaaa5c224664340252d964e6373220f3a7af21
deelakar <deelaka98@gmail.com>
2024-03-28 21:33:36 +1100

    Initial Commit

---
+++ SIT707-2.1P/2.1P/src/main/java/sit707_week2/Main.java
@@ -0,0 +1,12 @@
+package sit707_week2;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // To run officeworks_registration_page()
+         SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        
+        // To run alternative_registration_page()
+         SeleniumOperations.linkedin_registration_page("https://www.linkedin.com/signup");
+    }
+}
